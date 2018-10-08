2eeacb68ea0431ca5b61896803d857c4c1c56ffa
terencevanzoelen <terence@jaav.nl>
2018-10-08 23:31:42 +0200

    Update readme node

---
+++ page/README.ml
@@ -0,0 +1,13 @@
+### Webclient for the browser
+
+This package is built with webpack. The compiled entry is found in the `dist` folder.
+
+The `basic` folder contains a Bryntum scheduler component powered by the Jaav-tm framework.
+
+To use the plugin in your web app, just add the `main.js` to your page
+
+ <script src="../dist/main.js"></script>
+
+ This client is made online available on: http://5.157.85.76:3000/page and http://5.157.85.181:3000/page
+
+
