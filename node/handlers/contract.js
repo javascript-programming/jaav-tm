aeb5bcb41aca64d6814d782a4f04667afb7fa24a
terencevanzoelen <terence@jaav.nl>
2020-08-31 23:55:00 +0200

    Transform handlers into promises only wallet togo

--- node/handlers/contract.js
+++ node/handlers/contract.js
@@ -3,27 +3,31 @@ const TU = require('../common/transactionutils');
 
 function executeContract (contract, state, fn, params, account, value) {
 
-    if (contract.abi[fn]) {
+    return new Promise(async (resolve, reject) => {
+        if (contract.abi[fn]) {
 
-        const code = Buffer.from(contract.code, 'base64').toString();
-        const Cls = CU.getClass(code);
-        const instance = new Cls(state);
+            const code = Buffer.from(contract.code, 'base64').toString();
+            const Cls = CU.getClass(code);
+            const instance = new Cls(state);
 
-        instance.caller = account;
-        instance.value = value;
+            instance.caller = account;
+            instance.value = value;
 
-        return {
-            result : instance[fn].apply(instance, params),
-            state  : instance.state,
-            tags   : [
-                { key : Buffer.from('jv.contract'), value : Buffer.from(contract.address) },
-                { key : Buffer.from('jv.event'), value : Buffer.from(fn) }
-            ]
+            const fnRef = instance[fn];
+
+            resolve({
+                result : (fnRef instanceof Promise) ? await fnRef.apply(instance, params) : fnRef.apply(instance, params),
+                state  : instance.state,
+                tags   : [
+                    { key : Buffer.from('jv.contract'), value : Buffer.from(contract.address) },
+                    { key : Buffer.from('jv.event'), value : Buffer.from(fn) }
+                ]
+            });
+
+        } else {
+            reject('Function not found in contract');
         }
-
-    } else {
-        throw new Error('Function not found in contract');
-    }
+    });
 }
 
 class ContractHandler {
@@ -33,122 +37,154 @@ class ContractHandler {
     }
 
     static deploy_contract (state, tx) {
+        return new Promise(async (resolve, reject) => {
 
-        const account = state.getAccount(tx.account)
+            const account = await state.getAccount(tx.account);
 
-        if (!state.accounts[tx.account])
-            throw new Error('Contract can only be deployed from valid account');
-
-        if (state.contracts[tx.to] && state.contracts[tx.to].deployed)
-            throw new Error('This contract address is already deployed');
-
-        const params = tx.params;
-
-        if (params.index === 1) {
-            state.contracts[tx.to] = {
-                balance     : 0,
-                owner       : tx.account,
-                address     : tx.to,
-                cashbook    : [],
-                name        : tx.params.name,
-                abi         : tx.params.abi,
-                code        : '',
-                deployed    : false
-            };
-        }
-
-        const contract = state.contracts[tx.to];
-        contract.code += params.code;
-
-        if (params.index === params.total) {
-            const Cls = CU.getClass(Buffer.from(contract.code, 'base64').toString());
-            const instance = new Cls();
-            contract.state = instance.state || {};
-            contract.deployed = true;
-        }
-
-        return {
-            log     : 'Contract deployment',
-            result  : {
-                address     : contract.address,
-                abi         : contract.abi,
-                owner       : contract.owner,
-                deployed    : contract.deployed
+            if (!account) {
+                reject('Contract can only be deployed from valid account');
+                return;
             }
-        };
+
+            let contract = await state.getContract(tx.to);
+
+            if (contract && contract.deployed) {
+                reject('This contract address is already deployed');
+                return;
+            }
+
+            const params = tx.params;
+
+            if (params.index === 1) {
+                contract = {
+                    _id         : tx.to,
+                    balance     : 0,
+                    owner       : tx.account,
+                    address     : tx.to,
+                    cashbook    : [],
+                    name        : tx.params.name,
+                    abi         : tx.params.abi,
+                    code        : '',
+                    deployed    : false
+                };
+            }
+
+            contract.code += params.code;
+
+            if (params.index === params.total) {
+                const Cls = CU.getClass(Buffer.from(contract.code, 'base64').toString());
+                const instance = new Cls();
+                contract.state = instance.state || {};
+                contract.deployed = true;
+            }
+
+            await state.insertRecord(contract, 'contracts');
+
+            resolve({
+                log     : 'Contract deployment',
+                result  : {
+                    address     : contract.address,
+                    abi         : contract.abi,
+                    owner       : contract.owner,
+                    deployed    : contract.deployed
+                }
+            });
+        });
     }
 
     static call_contract (state, tx) {
 
-        const contract = state.contracts[tx.to];
+        return new Promise(async (resolve, reject) => {
+            const contract = await state.getContract(tx.to);
+            const account = await state.getAccount(tx.account);
 
-        if (contract) {
+            if (contract) {
 
-            if (!state.accounts[tx.account])
-                throw new Error('Caller account unknown');
+                if (!account)
+                    reject('Caller account unknown');
 
-            //todo check value and balance sender
-            const result = executeContract(contract, contract.state, tx.params.fn, tx.params.params, tx.account, tx.value);
-            return {
-                log     : 'Contract call executed',
-                result  : result.result,
-                tags    : result.tags
+                //todo check value and balance sender
+                const result = await executeContract(contract, contract.state, tx.params.fn, tx.params.params, tx.account, tx.value);
+                resolve({
+                    log     : 'Contract call executed',
+                    result  : result.result,
+                    tags    : result.tags
+                });
+            } else {
+                reject('Contract not found');
             }
-        } else {
-            throw new Error('Contract not found')
-        }
+        });
     }
 
     static query_contract (state, account, contract, fn, params) {
 
-        if (contract) {
+        return new Promise(async (resolve, reject) => {
+            if (contract) {
 
-            if (!state.accounts[account])
-                throw new Error('Caller account unknown');
+                const caller = await state.getAccount(account);
 
-            return executeContract(contract, contract.state, fn, params, account).result;
+                if (!caller) {
+                    reject('Caller account unknown');
+                    return;
+                }
 
-        } else {
-            throw new Error('Contract not found')
-        }
+                const result = await executeContract(contract, contract.state, fn, params, account);
+                resolve(result.result);
+
+            } else {
+                reject('Contract not found');
+            }
+        });
     }
 
 
     //code almost the same as in wallet
     static transfer_funds (state, tx) {
 
-        if (Number.isInteger(tx.value) && tx.value > 0) {
+        return new Promise(async (resolve, reject) => {
+            const fromContract = await state.getContract(tx.account);
 
-            if (!state.accounts[tx.to] && !state.contracts[tx.to]) {
-                throw new Error('Be happy! No funds are lost while you have sent your funds into the blue.');
-            }
+            if (Number.isInteger(tx.value) && tx.value > 0) {
 
-            if (state.contracts[tx.account].balance >= tx.value) {
+                let toAccount = await state.getAccount(tx.to) || await state.getContract(tx.to);
 
-                const receiver = state.accounts[tx.to] || state.contracts[tx.to];
+                if (!toAccount) {
+                    reject('Be happy! No funds are lost while you have sent your funds into the blue.');
+                    return;
+                }
 
-                receiver.balance += tx.value;
-                state.contracts[tx.account].balance -= tx.value;
+                if (fromContract.balance >= tx.value) {
 
-                let message = tx.params.message;
-                let cashRecord = { from: tx.account, amount: tx.value, message };
-                receiver.cashbook.push(cashRecord);
+                    toAccount.balance += tx.value;
+                    fromContract.balance -= tx.value;
 
-                cashRecord = { to: tx.to, amount: tx.value, message };
-                state.contracts[tx.account].cashbook.push(cashRecord);
+                    let message = tx.params.message;
+                    let cashRecord = { from: tx.account, amount: tx.value, message };
+                    toAccount.cashbook.push(cashRecord);
+
+                    cashRecord = { to: tx.to, amount: tx.value, message };
+                    fromContract.cashbook.push(cashRecord);
+
+                    await state.updateRecord(toAccount._id, toAccount, toAccount.abi? 'contracts' : 'accounts');
+                    await state.updateRecord(fromContract._id, fromContract, 'contracts');
+
+                } else {
+                    reject('Insufficient funds you have!');
+                    return;
+                }
 
             } else {
-                throw new Error('Insufficient funds you have!');
+                reject('Value should be positive integer');
+                return;
             }
 
-        } else {
-            throw new Error('Value should be positive integer');
-        }
+            resolve({
+                log     : 'Contract balance updated',
+                result  : fromContract.balance
+            });
+        });
 
-        return {
-            log     : 'Contract balance updated',
-            result  : state.contracts[tx.account].balance
-        };
+
     }
 
 }
